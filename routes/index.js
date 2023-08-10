72f32f952a1a7ce6e59f16d9469d18831ea7a43f
Boom <boomlee.dev@gmail.com>
2023-08-10 19:12:05 +0700

    create user profile and view their post. No CSS YET

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const isLoggedin = require('../middleware/routesprotector').userIsLoggedIn;
 const { getNRecentPosts, getPostById, getCommentsByPostId } = require('../middleware/postsmiddleware');
+const { getUserById } = require('../models/Users');
+const { getPostByUserId } = require('../models/Posts');
+
 // const db = require('../conf/database');
 
 router.get('/', getNRecentPosts, function (req, res, next) {
@@ -19,6 +22,16 @@ router.get('/registration', (req, res, next) => {
 router.use('postImage', isLoggedin);
 router.get('/postimage', (req, res, next) => {
     res.render('postimage', { title: 'Post Image' });
+});
+router.get('/profile', async (req, res, next) => {
+    const userId = req.session.userId;
+    const viewUser = await getUserById(userId);
+    const results = await getPostByUserId(userId);
+    res.render('profile', {
+        title: 'User Profile',
+        user: viewUser[0],
+        posts: results
+    });
 });
 
 // post/id
