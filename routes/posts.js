97ff060dbc7c7226f4c82c8910e437fb5f64414c
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-08-11 15:39:14 +0700

    add  feature post

--- routes/posts.js
+++ routes/posts.js
@@ -11,7 +11,13 @@ const { body, validationResult } = require('express-validator');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/images/uploads');
+        if (file.fieldname === 'uploadImage') {
+            cb(null, 'public/images/uploads');
+        } else if (file.fieldname === 'uploadVideo') {
+            cb(null, 'public/videos/uploads');
+        } else {
+            cb(new Error('Invalid fieldname'));
+        }
     },
     filename: function (req, file, cb) {
         const fileExt = file.mimetype.split('/')[1];
@@ -22,10 +28,13 @@ const storage = multer.diskStorage({
 
 const uploader = multer({ storage });
 
-router.post('/createPost', [body('title').isLength({ min: 0 }), body('description').isLength({ min: 0 })], uploader.single('uploadImage'), (req, res, next) => {
-    const fileUploaded = req.file.path;
-    const fileAsThumbnail = `thumbnail-${req.file.filename}`;
-    const destinationOfThumbnail = req.file.destination + '/' + fileAsThumbnail;
+router.post('/createPost', [body('title').isLength({ min: 0 }), body('description').isLength({ min: 0 })], uploader.fields([
+    { name: 'uploadImage', maxCount: 1 },
+    { name: 'uploadVideo', maxCount: 1 }
+]), (req, res, next) => {
+    const fileUploaded = req.files['uploadVideo'][0].path;
+    const fileAsThumbnail = `thumbnail-${req.files['uploadImage'][0].filename}`;
+    const destinationOfThumbnail = req.files['uploadImage'][0].destination + '/' + fileAsThumbnail;
     const title = req.body.title;
     const description = req.body.description;
     const fk_userId = req.session.userId;
@@ -37,7 +46,7 @@ router.post('/createPost', [body('title').isLength({ min: 0 }), body('descriptio
         return res.status(400).json({ errors: errors.array() });
     }
 
-    sharp(fileUploaded)
+    sharp(req.files['uploadImage'][0].path)
         .resize(200)
         .toFile(destinationOfThumbnail)
         .then(() => {
