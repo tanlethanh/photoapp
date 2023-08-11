fb84b347e1403f48b8a5b70666c04b90a3fe26dc
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-08-11 17:18:45 +0700

    fix post video

--- routes/posts.js
+++ routes/posts.js
@@ -1,52 +1,55 @@
-const express = require('express');
-const router = express.Router();
-// const db = require('../conf/database');
-const { errorPrint } = require('../helpers/debug/debugprinters');
-const sharp = require('sharp');
-const multer = require('multer');
-const crypto = require('crypto');
-const PostError = require('../helpers/error/PostError');
-const PostModel = require('../models/Posts');
+var express = require('express');
+var router = express.Router();
+var db = require('../conf/database');
+const { successPrint, errorPrint } = require('../helpers/debug/debugprinters');
+var sharp = require('sharp');
+var multer = require('multer');
+var crypto = require('crypto');
+var PostError = require('../helpers/error/PostError')
+var PostModel = require('../models/Posts')
 const { body, validationResult } = require('express-validator');
 
-const storage = multer.diskStorage({
+var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        if (file.fieldname === 'uploadImage') {
-            cb(null, 'public/images/uploads');
-        } else if (file.fieldname === 'uploadVideo') {
-            cb(null, 'public/videos/uploads');
+        if (file.fieldname === "uploadImage") {
+            cb(null, "public/images/uploads");
+        } else if (file.fieldname === "uploadVideo") {
+            cb(null, "public/videos/uploads");
         } else {
-            cb(new Error('Invalid fieldname'));
+            cb(new Error("Invalid fieldname"));
         }
     },
     filename: function (req, file, cb) {
-        const fileExt = file.mimetype.split('/')[1];
-        const randomName = crypto.randomBytes(22).toString('hex');
+        let fileExt = file.mimetype.split('/')[1];
+        let randomName = crypto.randomBytes(22).toString("hex");
         cb(null, `${randomName}.${fileExt}`);
     }
 });
 
-const uploader = multer({ storage });
 
-router.post('/createPost', [body('title').isLength({ min: 1 }), body('description').isLength({ min: 1 })], uploader.fields([
+var uploader = multer({ storage: storage });
+
+router.post("/createPost", [body('title').isLength({ min: 0 }), body('description').isLength({ min: 0 })], uploader.fields([
     { name: 'uploadImage', maxCount: 1 },
     { name: 'uploadVideo', maxCount: 1 }
 ]), (req, res, next) => {
-    const fileUploaded = req.files['uploadVideo'][0].path;
-    const fileAsThumbnail = `thumbnail-${req.files['uploadImage'][0].filename}`;
-    const destinationOfThumbnail = req.files['uploadImage'][0].destination + '/' + fileAsThumbnail;
-    const title = req.body.title;
-    const description = req.body.description;
-    const fk_userId = req.session.userId;
+
+    let fileUploaded = req.files["uploadVideo"][0].path;
+    let fileAsThumbnail = `thumbnail-${req.files["uploadImage"][0].filename}`;
+    let destinationOfThumbnail = req.files["uploadImage"][0].destination + "/" + fileAsThumbnail;
+    let title = req.body.title;
+    let description = req.body.description;
+    let fk_userId = req.session.userId;
 
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    if (!errors.isEmpty()) 
+    {
         req.flash('error', 'Post could not be made');
         res.redirect('/');
         return res.status(400).json({ errors: errors.array() });
     }
 
-    sharp(req.files['uploadImage'][0].path)
+    sharp(req.files["uploadImage"][0].path)
         .resize(200)
         .toFile(destinationOfThumbnail)
         .then(() => {
@@ -55,12 +58,12 @@ router.post('/createPost', [body('title').isLength({ min: 1 }), body('descriptio
                 description,
                 fileUploaded,
                 destinationOfThumbnail,
-                fk_userId
+                fk_userId,
             );
         })
         .then((postWasCreated) => {
             if (postWasCreated) {
-                req.flash('success', 'Post was created successfully');
+                req.flash('success', "Post was created successfully");
                 res.redirect('/');
             } else {
                 throw new PostError('Posts could not be created', 'postimage', 200);
@@ -75,29 +78,29 @@ router.post('/createPost', [body('title').isLength({ min: 1 }), body('descriptio
             } else {
                 next(err);
             }
-        });
+        })
 });
 
 router.get('/search', async (req, res, next) => {
     try {
-        const searchTerm = req.query.search;
+        let searchTerm = req.query.search;
         if (!searchTerm) {
             res.send({
-                message: 'no search term given',
-                results: []
+                message: "no search term given",
+                results: [],
             });
         } else {
-            const results = await PostModel.search(searchTerm);
+            let results = await PostModel.search(searchTerm);
             if (results && results.length) {
                 res.send({
                     message: `${results.length} results found`,
-                    results
+                    results: results
                 });
             } else {
-                const results = await PostModel.getNRecentPosts(8);
+                let results = await PostModel.getNRecentPosts(8);
                 res.send({
-                    message: 'no results were your search were found. showing 8 most recent posts',
-                    results
+                    message: "no results were your search were found. showing 8 most recent posts",
+                    results: results,
                 });
             }
         }
